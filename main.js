11f2b88685da46b4038e565b5c9405c79e728319
naz_liton <nazrulliton@icloud.com>
2019-12-31 14:04:02 +0000

    add currency func added

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 //global variables
 
 const addCurrencyBtn = document.querySelector(".add-currency-btn");
+const addcurrencyList = document.querySelector(".add-curency-list");
+const currencies = [
+  {
+    name: "US Dollar",
+    abbreviation: "USD",
+    symbol: "\u0024",
+    flagURL: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1920px-Flag_of_the_United_States.svg.png",
+  }
+];
+
 
 // Event Listeners
 
@@ -9,3 +19,17 @@ addCurrencyBtn.addEventListener("click", addCurrencyBtnClick);
 function addCurrencyBtnClick(event) {
   addCurrencyBtn.classList.toggle("open");
 }
+
+// Auxillary Functions
+
+function populateAddCyrrencyList() {
+  for (let i=0; i<currencies.length; i++){
+    addCurrencyList.insertAdjacentHTML("beforeend",
+    `<li data-currency="USD" class="disabled">
+    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1920px-Flag_of_the_United_States.svg.png" class="flag"><span>USD - US Dollar</span>
+    </li>`
+  );
+  }
+}
+
+populateAddCyrrencyList();
