c8ac3ffd7ee1d4fb8a14145c86718ee2094fb4de
naz_liton <nazrulliton@icloud.com>
2019-12-31 13:41:02 +0000

    eventlisten for add currency

---
+++ main.js
@@ -0,0 +1,11 @@
+//global variables
+
+const addCurrencyBtn = document.querySelector(".add-currency-btn");
+
+// Event Listeners
+
+addCurrencyBtn.addEventListener("click", addCurrencyBtnClick);
+
+function addCurrencyBtnClick(event) {
+  addCurrencyBtn.classList.toggle("open");
+}
